b0b7252b0de2c832c5746bc003a9389ae15d32aa
sther.redhat@gmail.com <sther.redhat@gmail.com>
2020-12-21 17:01:02 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const app = express();
+
+const routes = require('./routes/routes');
+
+app.use('/produtos', routes);
+
+module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+﻿const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+   res.status(200).send({
+    mensagem: 'usando outra rota'
+   });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'usando post de produtos'
+    });
+});
+
+router.post('/:valor', (req, res, next) => {
+const valor = req.params.valor;
+const result = valor * 2;
+    res.status(201).send({
+        mensagem: 'a multiplicação por dois do valor passado é:',
+        resultado: result
+    });
+});
+
+router.get('/hello-world', (req, res, next) => {
+    res.status(200).send({
+     mensagem: 'Hello World!!!'
+    });
+ });
+
+
+
+module.exports = router;
